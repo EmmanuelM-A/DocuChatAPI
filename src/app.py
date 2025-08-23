368af9f153ef963d36613c419f75405249a250ae
EmmanuelM-A <madukaagbeze.ea@gmail.com>
2025-08-23 21:45:14 +0100

    feat(db): integrated db init and shutdown into api startup and shutdown

--- src/app.py
+++ src/app.py
@@ -3,16 +3,31 @@ This module initializes the FastAPI application and includes the routes for
 the application.
 """
 
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
+from database.db_manager import get_database_manager
 from src.middleware.exception_handler import setup_exception_handlers
 from src.middleware.http_logging import HTTPLoggingMiddleware
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """Manage startup and shutdown tasks."""
+    db_manager = get_database_manager()
+
+    # Startup
+    await db_manager.setup_database()
+    yield
+    # Shutdown
+    await db_manager.shutdown()
 
 
 def create_app():
     """Create and configure the FastAPI application."""
 
-    app = FastAPI()
+    app = FastAPI(lifespan=lifespan)
 
     # Middlewares
     app.add_middleware(HTTPLoggingMiddleware)
